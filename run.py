97fd02c2c52d0713afa9111b99985a6ef4c1f680
shubhamtibra <shubh.tibra@gmail.com>
2020-07-14 21:43:26 +0530

    added the logic for movie selection

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, make_response, jsonify
 from datetime import datetime
 
 app = Flask(__name__)
@@ -10,9 +10,24 @@ def home():
 @app.route('/submit', methods=['POST'])
 def submit():
   data = request.get_json()
-  movies = data['Movie Name']
   sdate = data['Start Date']
   fdate = data['End Date']
-  return 'You entered: {}'.format(request.get_json())
+  sdate = [datetime.strptime(date, "%d %b") for date in sdate]
+  fdate = [datetime.strptime(date, "%d %b") for date in fdate]
+  movies = list(zip(data['Movie name'], sdate, fdate))
+  if(len(movies) == 0):
+    return make_response(jsonify({}), 200)
+  movies.sort(key=lambda x:x[2])
+  selected = [movies[0][0]]
+  i = 0
+  for j in range(1, len(movies)):
+    if movies[j][1] >= movies[i][2]:
+        selected.append(movies[j][0])
+        i=j
+
+  res = {"Maximum Money" : '{} Crores'.format(len(selected)), "Selected Movies":selected}
+
+  return make_response(jsonify(res), 200)
+  #return 'You entered: {}'.format(request.get_json())
 
 app.run(host="localhost", debug=True)
